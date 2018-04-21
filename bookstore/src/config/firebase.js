9fcb7d167fccd19d92e51d1fc399921d65eb8b7e
ernesto.liberio@gmail.com <ernesto.liberio@gmail.com>
2018-04-21 16:24:47 -0500

    webpack

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyAtczmTapCrgYgZ0sq5XLgZpq9Z-2wpZCI",
+    authDomain: "bookstore-68e16.firebaseapp.com",
+    databaseURL: "https://bookstore-68e16.firebaseio.com",
+    projectId: "bookstore-68e16",
+    storageBucket: "bookstore-68e16.appspot.com",
+    messagingSenderId: "767347056419"
+};
---
+++ bookstore/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from '@/config/firebase';
+firebase.initializeApp(firebaseConfig); 
+
+export const db=firebase.firestore(); 
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
