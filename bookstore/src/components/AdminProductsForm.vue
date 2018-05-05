7d56a99b01f073a5feb1e8f22d31ecfad18be72d
ernesto.liberio@gmail.com <ernesto.liberio@gmail.com>
2018-05-05 14:47:35 -0500

    adminProduct inicio

--- bookstore/src/components/AdminProductsForm.vue
+++ bookstore/src/components/AdminProductsForm.vue
@@ -1,6 +1,6 @@
 <template>
  <v-layout row justify-center>
-    <v-dialog persistent max-width="510">
+    <v-dialog persistent max-width="510" v-model="productsDialog">
       <v-btn slot="activator" color="light-blue darken-4" dark>
       	{{ $t('admin.productsTable.newProduct') }}
       </v-btn>
@@ -10,13 +10,23 @@
         	<v-container grid-list-md>
         		<v-layout wrap>
         			<v-flex>
-        				<v-text-field :label="$t('admin.productsTable.name')"/>
+        				<v-text-field :label="$t('admin.productsTable.name')"
+        				v-model="productForEdit.name"
+        				/>
         			</v-flex>
         			<v-spacer/>
         			<v-flex>
-        				<v-text-field :label="$t('admin.productsTable.price')"/>
+        				<v-text-field :label="$t('admin.productsTable.price')" 
+						v-model="productForEdit.price"
+        				/>
         			</v-flex>
         			<v-spacer/>
+        			<v-flex xs12> 
+        				<a v-if="productForEdit.url" 
+        				  :href="productForEdit.url" target="_blank">
+        				{{ $t('admin.productsTable.openFile') }}</a>
+						<file-input accept="image/*" @input="getUploadFile"/>
+        			</v-flex>
 
         		</v-layout>
         	</v-container>
@@ -26,19 +36,65 @@
           <v-btn color="green darken-1" flat @click="cerrar">
           	{{$t('common.close')}}
           </v-btn>
-          <v-btn color="green darken-1" flat>Agree</v-btn>
+          <v-btn color="green darken-1" flat 
+                  v-if="$store.getters.productsDialogEditMode">Actualizar</v-btn>
+          <v-btn  v-else color="green darken-1" @click="add" flat>Agregar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
  </v-layout>
 </template>
 <script>
+import {db} from "@/main";
+import * as faker from 'faker';
+import {mapGetters} from 'vuex';
+import FileInput from "@/components/helpers/FileInput";
 	export default{
 		name:"admin-products-form",
+		components:{FileInput},
 		methods:{
 			cerrar(){
-				alert('cerrando');
+				this.$store.commit('toggleProductsDialog',{
+					editMode:false,
+					product:{
+						id:null,
+						name:'',
+						price:'',
+						url:'',
+						file_id:''
+					}
+				})
+			},
+			add(){
+				this.productForEdit.id=faker.random.alphaNumeric(16);
+				const product=Object.assign({},this.productForEdit);
+				product.createdAt=Date.now();
+				db.collection('products').doc(this.productForEdit.id)
+					.set(product).then(()=>{
+						if(this.image){
+							this.$store.dispatch('pushFileToStorage',{
+								fileToUpload:this.image,id:product.id
+							}).then(()=>{
+								alert('guardado');
+							})
+						}else{
+							alert('guardado');
+						}
+					});
 			}
-		}
+		},
+		computed:{
+			productsDialog:{
+				get(){
+					return this.$store.getters.productsDialog;
+				},
+				set(){
+					this.close();
+				}
+			},
+			...mapGetters(['productForEdit'])
+		},
+
+		
 	}
 </script>
